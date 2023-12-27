ca79375c7eb664675ea0c95d6fcea3a25d1bd050
riskiana <tell.riskiana@gmail.com>
2023-12-27 09:31:42 +0700

    display authenticated user

--- src/main/java/com/ubl/studentweb/rest/StudentController.java
+++ src/main/java/com/ubl/studentweb/rest/StudentController.java
@@ -8,6 +8,9 @@ import java.util.Optional;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -31,6 +34,11 @@ public class StudentController {
 
     @GetMapping("/students")
     public String getStudents(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        model.addAttribute("authenticatedUser", currentPrincipalName);
         model.addAttribute("students", studentService.getStudents());
         return "index";
     }
